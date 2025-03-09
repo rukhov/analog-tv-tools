69ed25a8014ae9d1576cc8d041c9547b8d9eb7b3
Roman Ukhov <ukhov.roman@gmail.com>
2025-03-09 12:17:37 +0200

    Revert "-"
    This reverts commit b0465f349d0b155da1ff0113240ed70d542f2356.

--- scripts/windows/kill-dialogs.ps1
+++
@@ -1,9 +0,0 @@
-Write-Output "Killing all the visual c++ runtime dialogs"
-
-$Title="Microsoft Visual C++ Runtime Library"
-
-while ($true) {
-    Start-Sleep -Seconds 2
-	Get-Process | Where-Object { $_.MainWindowTitle -eq $Title } | ForEach-Object { $_.Kill() }
-}
-
